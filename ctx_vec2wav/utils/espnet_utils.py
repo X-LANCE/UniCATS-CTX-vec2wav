465dd90b8d8f4029497c53c8e0f3b9e276aa8ede
Yiwei Guo <58417810+cantabile-kwok@users.noreply.github.com>
2024-01-30 20:24:14 +0800

    Update espnet_utils.py

--- ctx_vec2wav/utils/espnet_utils.py
+++ ctx_vec2wav/utils/espnet_utils.py
@@ -490,7 +490,7 @@ def rename_state_dict(
 def get_activation(act):
     """Return activation function."""
     # Lazy load to avoid unused import
-    from espnet.nets.pytorch_backend.conformer.swish import Swish
+    from ctx_vec2wav.models.conformer.swish import Swish
 
     activation_funcs = {
         "hardtanh": torch.nn.Hardtanh,
